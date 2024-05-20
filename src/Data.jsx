8e84fead6d40482e9e5aa3e2a5dbf935f798bb71
iamharikrishnaa <iamharikrishnaa@gmail.com>
2024-05-20 21:36:10 +0530

    sdcms

---
+++ src/Data.jsx
@@ -0,0 +1,174 @@
+import { FiPenTool } from "react-icons/fi";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsBarChartLine } from "react-icons/bs";
+import { MdOutlineScience } from "react-icons/md";
+
+import courses1 from "./assets/courses1.jpg";
+import courses2 from "./assets/courses2.jpg";
+import courses3 from "./assets/courses3.jpg";
+import courses4 from "./assets/courses4.jpg";
+import courses5 from "./assets/courses5.jpg";
+import courses6 from "./assets/courses6.jpg";
+import courses7 from "./assets/courses7.jpg";
+import courses8 from "./assets/courses8.jpg";
+
+import logo1 from "./assets/client1.jpg";
+import logo2 from "./assets/client2.jpg";
+import logo3 from "./assets/client3.png";
+import logo4 from "./assets/client4.png";
+import logo5 from "./assets/client5.png";
+import logo6 from "./assets/client6.png";
+import logo7 from "./assets/client7.png";
+import logo8 from "./assets/client8.png";
+import logo9 from "./assets/client9.png";
+import logo10 from "./assets/client10.jpg";
+import logo11 from "./assets/client11.jpg";
+import logo12 from "./assets/client12.jpg";
+import logo13 from "./assets/client13.png";
+
+export const navLinks = [
+  {
+    id: 1,
+    href: "home",
+    link: "Home",
+  },
+  {
+    id: 2,
+    href: "projects",
+    link: "Projects",
+  },
+  {
+    id: 3,
+    href: "services",
+    link: "Services",
+  },
+  {
+    id: 4,
+    href: "about",
+    link: "About",
+  },
+  {
+    id: 5,
+    href: "contact",
+    link: "Contact",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    icon: <FiPenTool />,
+    category: "Design",
+  },
+  {
+    id: 2,
+    icon: <FaLaptopCode />,
+    category: "Development",
+  },
+  {
+    id: 3,
+    icon: <BsBarChartLine />,
+    category: "Business",
+  },
+  {
+    id: 4,
+    icon: <MdOutlineScience />,
+    category: "Science",
+  },
+];
+
+export const courses = [
+  {
+    id: 1,
+    image: courses1,
+    category: "Web Design",
+    title: "The Complete Web Design Course",
+    rating: 4.9,
+    participants: 500,
+    price: 105,
+  },
+  {
+    id: 2,
+    image: courses2,
+    category: "Web Development",
+    title: "FullStack Web Development Course",
+    rating: 4.8,
+    participants: 700,
+    price: 125,
+  },
+  {
+    id: 3,
+    image: courses3,
+    category: "Web Development",
+    title: "Python Beginner to Advanced Course",
+    rating: 4.9,
+    participants: 300,
+    price: 99,
+  },
+  {
+    id: 4,
+    image: courses4,
+    category: "Web Design",
+    title: "UI/UX Design Crazy Course With Harrys",
+    rating: 4.7,
+    participants: 600,
+    price: 85,
+  },
+  {
+    id: 5,
+    image: courses5,
+    category: "AI Development",
+    title: "AI Development With Python (complete beginner course)",
+    rating: 4.9,
+    participants: 500,
+    price: 105,
+  },
+  {
+    id: 6,
+    image: courses6,
+    category: "Web Design",
+    title: "The Complete Web Design Course",
+    rating: 4.9,
+    participants: 500,
+    price: 105,
+  },
+  {
+    id: 7,
+    image: courses7,
+    category: "Web Design",
+    title: "The Complete Web Design Course",
+    rating: 4.9,
+    participants: 500,
+    price: 105,
+  },
+  {
+    id: 8,
+    image: courses8,
+    category: "Web Design",
+    title: "The Complete Web Design Course",
+    rating: 4.9,
+    participants: 500,
+    price: 105,
+  },
+];
+
+export const logos = [logo1, logo2, logo3, logo4, logo5, logo6];
+
+export const accordions = [
+  {
+    id: 1,
+    title: "What is Skillex?",
+  },
+  {
+    id: 2,
+    title: "What can I learn from Skillex?",
+  },
+  {
+    id: 3,
+    title: "Can I teach on Skillex?",
+  },
+  {
+    id: 4,
+    title: "What is included in my Skillex membership?",
+  },
+];
